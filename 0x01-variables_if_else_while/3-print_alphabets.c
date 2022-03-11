0372dc1eb79e44f60d8eaa572bd4c46b630c536a
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-11 02:01:01 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/*
- * header at the first line
- * and followed by main
- * function
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ascii_code;
+
 	for (ascii_code = 97; ascii_code <= 122; ascii_code++)
 		putchar(ascii_code);
 	for (ascii_code = 65; ascii_code <= 90; ascii_code++)
