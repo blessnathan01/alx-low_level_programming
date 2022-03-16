75a6aa906242fdabc178e7504947514b39495fc6
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-16 12:00:09 -0700

    Return value and print a sign of a given number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,28 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * print_sign - function to print sign
- * @c:  is the int that will use for the argument of the function
- * Return: 1 if lowercase or uppercase character 0 if not
+ * @n: number to be checked
+ * Return: 1 and print + if n > 0
+ * Retrun: 0 and print 0 if n == 0
+ * Return: -1 and print - if n < 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(n);
+		_putchar('+');
 		return (1);
 
 	}
 	else if (n == 0)
 	{
-		_putchar(n);
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar(n);
+		_putchar('-');
 		return (-1);
 	}
 }
