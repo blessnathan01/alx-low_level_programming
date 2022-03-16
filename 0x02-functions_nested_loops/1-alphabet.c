4115800af23a9e7fbd86dd7b3a89ba6724b6ca56
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-15 17:42:24 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,12 @@
 #include "main.h"
 
-void ptint_alphabet(void)
+void print_alphabet(void)
 {
-	char ch1;
-	char ch2;
-	char ch3;
-	char ch4;
-	char ch5;
-	char ch6;
-	char ch7;
-	char ch8;
-	char ch9;
+	int i;
 
-	ch1 = 'a';
-	ch2 = 'b';
-	ch3 = 'c';
-	ch4 = 'd';
-	ch5 = 'e';
-	ch6 = 'f';
-	ch7 = 'g';
-	ch8 = 'h';
-	ch9 = '\n';
-	_putchar(ch1);
-	_putchar(ch2);
-	_putchar(ch3);
-	_putchar(ch4);
-	_putchar(ch5);
-	_putchar(ch6);
-	_putchar(ch7);
-	_putchar(ch8);
-	_putchar(ch9);
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
 }
-
-#include "1-main.c"
