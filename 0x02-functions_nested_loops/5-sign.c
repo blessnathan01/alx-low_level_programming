7d97315b70972fced8cb6514ea6815c1cb2ba5b8
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-16 12:02:04 -0700

    Return value and print a sign of a given number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,8 +4,8 @@
  * print_sign - function to print sign
  * @n: number to be checked
  * Return: 1 and print + if n > 0
- * Retrun: 0 and print 0 if n == 0
- * Return: -1 and print - if n < 0
+ * 0 and print 0 if n == 0
+ * -1 and print - if n < 0
  */
 int print_sign(int n)
 {
