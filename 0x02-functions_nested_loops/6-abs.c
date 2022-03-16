9513830e1449e9171a16ea5474a6cad74c4d233c
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-15 19:59:49 -0700

    Deal with abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - if the character is alphabet
+ * @c:  is the int that will use for the argument of the function
+ * Return: 1 if lowercase or uppercase character 0 if not
+ */
+int _abs(int)
+{
+	abs(int);
+	return (0);
+}
