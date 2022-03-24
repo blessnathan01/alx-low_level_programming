1a0a05287dc9261836a2bb7b9d3b52fab4125343
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-24 12:32:52 -0700

    Add task 5 files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ *
+ * Return: string
+ */
+char *string_toupper(char *result)
+{
+	int len_string;
+
+	len_string = 0;
+	while (result[len_string] != '\0')
+	{
+		if (result[len_string] >= 97 && result[len_string] <= 122)
+			result[len_string] = result[len_string] - 32;
+		len_string++;
+	}
+	return (result);
+}
