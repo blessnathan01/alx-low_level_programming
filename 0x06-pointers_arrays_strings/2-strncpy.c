ebd5a4e5008ed3997ea88482f2f105943064591b
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-23 19:16:58 -0700

    Add task 2 files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * count till all bytes are written
+ * @dest: pointer to a string
+ * @src: pointer to a string
+ * @i: index
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count_byte;
+
+	for (count_byte = 0; count_byte < n && src[count_byte] != '\0'; count_byte++)
+	{
+		dest[count_byte] = src[count_byte];
+		for (; count_byte < n; count_byte++)
+		{
+			dest[count_byte] = '\0';
+		}
+	}
+	return (dest);
+}
