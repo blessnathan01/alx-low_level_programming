a9a4c47039eb1483fe2c005219827513f321bb75
blessnathan01 <blessingnathaniel01@gmail.com>
2022-04-01 14:30:57 -0700

    Write a c program to multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array of the command line arguments
+ *
+ * Return: Always 0.
+ * 1 when there is an error
+ */
+int main(int argc, char *argv[])
+{
+	int i, prod;
+
+	prod = 1;
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			prod *= atoi(argv[i]);
+		}
+		printf("%d\n", prod);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}	
+	return (0);
+}
