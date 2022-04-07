c8376a0d8cbb3edca27c914f1eec7ff883cdad9a
blessnathan01 <blessingnathaniel01@gmail.com>
2022-04-07 06:56:29 -0700

    Allocate memory space using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: length
+ *
+ * Return: a pointed to the allocated memory
+ * on failure, 98 is returned
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *v;
+
+	v = malloc(b);
+	if (v == NULL)
+		exit (98);
+	return (v);
+}
