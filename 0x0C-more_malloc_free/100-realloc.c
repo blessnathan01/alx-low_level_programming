ab40b0599c47b19ebd89d31e2e75af589b66bea9
blessnathan01 <blessingnathaniel01@gmail.com>
2022-04-07 10:01:12 -0700

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *nptr;
 	unsigned int i;
 
-	if(new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
 	if ((new_size == 0) && (ptr != NULL))
 	{
