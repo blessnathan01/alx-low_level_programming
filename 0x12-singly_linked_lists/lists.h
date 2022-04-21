abba13bda0369bc6794929f388f67869be0e3f41
blessnathan01 <blessingnathaniel01@gmail.com>
2022-04-21 15:06:16 -0700

    Update lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,8 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void print(void) __attribute__((constructor));
 
 #endif /* LISTS_H */
