c639ccbeaf7b5e7a2109656836a740e3fb441c4e
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-09 18:48:19 -0700

    Add 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,3 +27,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+unsigned int dlistint_len2(dlistint_t *h);
